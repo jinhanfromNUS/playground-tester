1b476cb12a4f2681e5d1864b05d7896714876f38
Source Academy User <No public email provided>
2022-10-20 19:15:14 +0800

    Changes made from Source Academy

--- stream to list and list itself.js
+++ stream to list and list itself.js
@@ -18,33 +18,33 @@ function skip_by_3_stream(start, end) {
            : pair(start, () => skip_by_3_stream(start + 3, end));
 }
 
-// skip_by_3_stream(8, 100);
+skip_by_3_stream(8, 100);
 eval_stream(skip_by_3_stream(8, 100), 3);
 
-// function skip_by_3_by_num(initial, n) {
-//     return n === 0
-//           ? null
-//           : pair(initial, skip_by_3_by_num(initial + 3, n - 1));
-// }
+function skip_by_3_by_num(initial, n) {
+    return n === 0
+          ? null
+          : pair(initial, skip_by_3_by_num(initial + 3, n - 1));
+}
 
-// skip_by_3_by_num(2, 30);
+skip_by_3_by_num(2, 30);
 
-// //filter using stream method
-// function filter_with_stream(start, end) {
-//     return stream_filter(x => x%5 === 0, enum_stream(start, end));
-// }
+//filter using stream method
+function filter_with_stream(start, end) {
+    return stream_filter(x => x%5 === 0, enum_stream(start, end));
+}
 
-// eval_stream(filter_with_stream(2, 20000), 5);
+eval_stream(filter_with_stream(2, 20000), 5);
 
-// //filter using list method
-// function filter_with_list(start, end) {
-//     return filter(x => x%5 === 0, enum_list(start, end));
-// }
+//filter using list method
+function filter_with_list(start, end) {
+    return filter(x => x%5 === 0, enum_list(start, end));
+}
 
-// function take(n, list) {
-//     return n === 0
-//           ? null
-//           : pair(head(list), take(n-1, tail(list)));
-// }
+function take(n, list) {
+    return n === 0
+          ? null
+          : pair(head(list), take(n-1, tail(list)));
+}
 
-// take(5, filter_with_list(5, 200000));
+take(5, filter_with_list(5, 200000));
