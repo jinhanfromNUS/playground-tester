f218f3985aede6aa9a9669b953d23fae95f259c7
Source Academy User <No public email provided>
2022-10-20 15:28:51 +0800

    enum stream and enum list outputting the same result

---
+++ stream to list and list itself.js
@@ -0,0 +1,20 @@
+
+
+// function test1(start, end) {
+//     return stream_filter(x => x%5 === 0, enum_stream(start, end));
+// }
+
+// eval_stream(test1(2, 20000), 5);
+
+// function test2(start, end) {
+//     return filter(x => x%5 === 0, enum_list(start, end));
+// }
+
+// function take(n, list) {
+//     return n === 0
+//           ? null
+//           : pair(head(list), take(n-1, tail(list)));
+// }
+
+
+// take(5, test2(5, 20000);
