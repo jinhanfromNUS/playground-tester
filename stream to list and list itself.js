610ebbac77061792ffe6b13541474cefbf1d274f
Source Academy User <No public email provided>
2022-10-20 15:34:35 +0800

    Changes made from Source Academy

--- stream to list and list itself.js
+++ stream to list and list itself.js
@@ -1,20 +1,20 @@
 
+//filter using stream method
+function filter_with_stream(start, end) {
+    return stream_filter(x => x%5 === 0, enum_stream(start, end));
+}
 
-// function test1(start, end) {
-//     return stream_filter(x => x%5 === 0, enum_stream(start, end));
-// }
+eval_stream(filter_with_stream(2, 20000), 5);
 
-// eval_stream(test1(2, 20000), 5);
+//filter using list method
+function filter_with_list(start, end) {
+    return filter(x => x%5 === 0, enum_list(start, end));
+}
 
-// function test2(start, end) {
-//     return filter(x => x%5 === 0, enum_list(start, end));
-// }
+function take(n, list) {
+    return n === 0
+          ? null
+          : pair(head(list), take(n-1, tail(list)));
+}
 
-// function take(n, list) {
-//     return n === 0
-//           ? null
-//           : pair(head(list), take(n-1, tail(list)));
-// }
-
-
-// take(5, test2(5, 20000);
+take(5, filter_with_list(5, 200000));
