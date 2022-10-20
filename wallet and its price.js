f9b7b3bae0c41b6972061f296875a6b88b435750
Source Academy User <No public email provided>
2022-10-20 11:24:46 +0800

    First version

---
+++ wallet and its price.js
@@ -0,0 +1,42 @@
+// Type your program in here!
+
+function tomato(n) {
+    return 20 * n;
+}
+    
+function cabbage(n) {
+    return 10 * n;
+}
+
+function add(n) {
+    return n;
+}
+
+// function make_wallet(amount) {
+//     let wallet = amount;
+//     return wallet = amount;
+// }
+
+let wallet = 100;
+wallet;
+
+wallet = wallet - tomato(2);
+wallet;
+
+wallet = wallet - tomato(2);
+wallet;
+
+wallet = wallet + add(100);
+wallet;
+
+
+/*
+intention
+create a budget_list to store all the transaction history within the list
+*/
+
+
+
+
+
+
